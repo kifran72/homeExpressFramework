917631398aed1189ba30c3cbd63a6683edac7cfc
kifran72 <cr91700kifran@gmail.com>
2022-01-13 16:43:07 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,4 @@
 const Routes = (app) => {
-  app.get("/products/:id", (req, res) => {
-    // for route /products/1, req.params has value  { id: '1' }
-  });
-  app.get("/products/", (req, res) => {
-    // for route /products?page=1&pageSize=10, req.query has value  { page: '1', pageSize: '10' }
-  });
-  app.post("/products/", (req, res) => {
-    // req.body should contain whatever you sent across as client
-  });
   app.get("/", (req, res) => {
     res.send("test");
   });
